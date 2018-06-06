4f19a391d3e0932d0d918c04af26a70db9fb3764
Andrew Kemp <andrew@ankemp.com>
2018-06-06 09:45:42 -0400

    Added/Setup firebase & @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCbxOSWoctDpt-LhRzvi0GrmXKjKggAXas',
+    authDomain: 'dauntless-builds.firebaseapp.com',
+    databaseURL: 'https://dauntless-builds.firebaseio.com',
+    projectId: 'dauntless-builds',
+    storageBucket: 'dauntless-builds.appspot.com',
+    messagingSenderId: '924945090932'
+  }
 };
 
 /*
